60b2d6ac1c2bb48b595c9c156dbd58d3389a493c
Anthoni Setiawan (ID) <anthoni.setiawan@Toni-Ganteng.local>
2020-04-20 16:03:51 +0700

    create upload image service

--- routes/SignUpUser.js
+++ routes/SignUpUser.js
@@ -2,16 +2,29 @@ const express = require("express");
 const Router = express.Router();
 const mysqlConnection = require("../connection");
 const bcrypt = require("bcrypt");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname.replace(/\s/g, ""));
+  },
+});
+const upload = multer({ storage: storage });
+// const upload = multer({ dest: "uploads/" });
 const saltRounds = 10;
-Router.post("/", (req, res) => {
+Router.post("/", upload.any("img"), (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
-  let img = req.body.img;
+  let img = req.files[0].path.replace(/\s/g, "");
   let role = req.body.role;
   let name = req.body.name;
   let phoneNumber = req.body.phone_number;
+  console.log(img);
+
   if (role === "tourist") {
-    bcrypt.hash(password, saltRounds, function(err, hash) {
+    bcrypt.hash(password, saltRounds, function (err, hash) {
       mysqlConnection.query(
         `INSERT INTO tourist (email, name, password, img, phone_number, role) VALUES ("${email}","${name}","${hash}","${img}","${phoneNumber}","${role}")`,
         (err, rows, fields) => {
@@ -19,15 +32,16 @@ Router.post("/", (req, res) => {
             res.send("Data Anda berhasil disimpan");
             console.log("Data Wisatawan berhasil disimpan");
           } else {
+            res.send("Email Telah Digunakan");
             console.log(err);
           }
         }
       );
     });
   } else if (role === "tourguide") {
-    bcrypt.hash(password, saltRounds, function(err, hash) {
+    bcrypt.hash(password, saltRounds, function (err, hash) {
       mysqlConnection.query(
-        `INSERT INTO tour_guide (email, name, password, img, phone_number, role) VALUES ("${email}","${name}","${hash}","${img}","${phoneNumber}","${role}")`,
+        `INSERT INTO tour_guide (email, name, password, img, phone_number) VALUES ("${email}","${name}","${hash}","${img}","${phoneNumber}")`,
         (err, rows, fields) => {
           if (!err) {
             res.send("Data Anda berhasil disimpan");
@@ -39,9 +53,6 @@ Router.post("/", (req, res) => {
         }
       );
     });
-  } else {
-    res.send("Error");
-    console.log("error");
   }
 });
 module.exports = Router;
