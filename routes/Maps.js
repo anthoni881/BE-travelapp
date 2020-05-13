d02786d0343d4f96c98458517b7249cf582bb12f
Anthoni Setiawan (ID) <anthoni.setiawan@Toni-Ganteng.local>
2020-05-13 14:07:11 +0700

    create service for maps

---
+++ routes/Maps.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const Router = express.Router();
+const mysqlConnection = require("../connection");
+
+Router.post("/", async (req, res) => {
+  const long = req.body.long;
+  const lat = req.body.lat;
+  
+    mysqlConnection.query(
+      "SELECT `id`,`name`, `email`, `img`, `phone_number`, `description`, `longitude`, `latitude`, ACOS( SIN( RADIANS( `latitude` ) ) * SIN( RADIANS( '" + lat + "' ) ) + COS( RADIANS( `latitude` ) ) * COS( RADIANS( '" + lat + "' )) * COS( RADIANS( `longitude` ) - RADIANS( '" + long + "' )) ) * 6380 AS `distance` FROM `tour_guide` WHERE ACOS( SIN( RADIANS( `latitude` ) ) * SIN( RADIANS( '" + lat + "' ) ) + COS( RADIANS( `latitude` ) ) * COS( RADIANS( '" + lat + "' )) * COS( RADIANS( `longitude` ) - RADIANS( '" + long + "' )) ) * 6380 < 10 ORDER BY `distance` ",
+      function(error, results, fields) {
+        console.log(results);
+        res.send(results)
+      }
+    );
+});
+module.exports = Router;
